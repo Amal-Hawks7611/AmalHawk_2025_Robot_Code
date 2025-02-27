bd8b94d6b78d36c43ee3d32f335c40d1118dca1a
AmalHawks7611 <127049757+AmalHawks7611@users.noreply.github.com>
2025-02-27 12:47:41 +0300

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -167,7 +167,7 @@ public final class Constants {
 
     public static class EnabledParts {
         // Hope Not To Use...
-        public static boolean IS_LED_ENABLED = false;
+        public static boolean IS_LED_ENABLED = true;
         public static boolean IS_OBJECT_SENSOR_ENABLED = false;
         public static boolean IS_INTAKE_ENABLED = true;
         public static boolean IS_ALGEA_INTAKE_ENABLED = true;
