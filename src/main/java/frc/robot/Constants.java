1faf988a091968a14ec3adaafe780bbcbdf31617
mehmet-2023 <mefeocal455@gmail.com>
2025-05-09 20:57:26 +0300

    Tuned everything :(
    Signed-off-by: mehmet-2023 <mefeocal455@gmail.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -29,8 +29,8 @@ public final class Constants {
 
     public static final class DrivebaseConstants {
         public static final double WHEEL_LOCK_TIME = 10;
-        public static final PIDConstants transation = new PIDConstants(0.1, 0.0, 0.05);
-        public static final PIDConstants angle = new PIDConstants(5, 0.0, 0.05);
+        public static final PIDConstants transation = new PIDConstants(8, 0.0, 0.05);
+        public static final PIDConstants angle = new PIDConstants(2, 0.0, 0.05);
     }
 
     public static class OperatorConstants {
